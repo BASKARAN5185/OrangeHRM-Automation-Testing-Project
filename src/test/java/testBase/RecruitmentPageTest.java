ab9ca3da5420f068b3947301fd4dd29a0bb25bf1
BASKARAN5185 <baskarbala5185@gmail.com>
2025-09-24 22:13:33 +0530

    Add basic tests for Recruitment Page functionality including login, navigation, and candidate form actions

--- src/test/java/testBase/RecruitmentPageTest.java
+++ src/test/java/testBase/RecruitmentPageTest.java
@@ -16,32 +16,69 @@ public class RecruitmentPageTest extends BaseClass {
 	OrangeHRMLoginPage lg = new OrangeHRMLoginPage(driver);
 	SoftAssert soft = new SoftAssert();
 
-	@Test(groups = { "smoke", "regression" })
+	@Test(groups = { "smoke", "regression" }, priority = 1)
 	void loginWithRecruitmentMenuClick() {
 		lg.login("Admin", "admin123");
 		String url = getCurrentUrlpage();
-		assertTrue(url.contains("/dashboard"), "Login the Appication is Failed");
+		assertTrue(url.contains("/dashboard"), "Login to Application failed");
 		String getUrl = rec.requirementMenuClick();
-		assertTrue(getUrl.contains("/viewCandidates"), "Recruitment Page navigation is failed");
+		assertTrue(getUrl.contains("/viewCandidates"), "Recruitment Page navigation failed");
 	}
 
-	@Test(dependsOnMethods = "loginWithRecruitmentMenuClick", groups = { "smoke", "regression" })
+	@Test(dependsOnMethods = "loginWithRecruitmentMenuClick", groups = { "smoke", "regression" }, priority = 2)
 	void candidateHeaderClick() {
 		String getUrl = rec.headersMenus("candidates");
-		assertTrue(getUrl.contains("/viewCandidates"), "Recruitment Page navigation is failed");
+		assertTrue(getUrl.contains("/viewCandidates"), "Recruitment Page navigation failed");
 	}
 
-	@Test(dependsOnMethods = "candidateHeaderClick", groups = { "regression", "sanity" })
-	void candidateVacanyfromdropdownoptionslection() {
+	@Test(dependsOnMethods = "candidateHeaderClick", groups = { "regression", "sanity" }, priority = 3)
+	void candidateVacancyFromDropdownOptionSelection() {
 		soft.assertEquals("Automaton Tester", rec.candidateJobTitleSelection("Automaton Tester"),
-				"The selected in wrong job title option");
+				"The selected job title option is wrong");
 		soft.assertEquals("Junior Account Assistant", rec.vacancySelection("Junior Account Assistant"),
-				"The selected in wrong vacancy option");
-		assertEquals(("Salma Reyes"), rec.hiringManagerSelection("Salma Reyes"),
-				"The selected in wrong hiring manager name option");
+				"The selected vacancy option is wrong");
 		soft.assertEquals("Interview Scheduled", rec.statusSelection("Interview Scheduled"),
-				"the selected in wrong status option");
+				"The selected status option is wrong");
 		soft.assertAll();
 	}
 
+	@Test(groups = { "smoke", "sanity" }, priority = 4)
+	void candidateNameEnter() {
+		String getEnterName = rec.candidateNameEnter("Guna");
+		assertEquals("Guna", getEnterName, "Entered Name is wrong");
+	}
+
+	@Test(groups = { "smoke", "regression" }, priority = 5)
+	void candidateKeywordEnter() {
+		assertEquals("Java", rec.keywordEnter("Java"), "Keyword is wrong");
+	}
+
+	@Test(groups = { "sanity" }, priority = 6)
+	void candidateApplicationFromAndToDateEnter() {
+		assertEquals("20-09-2025", rec.candidateApplicationFromDatePickup("20-09-2025"), "From Date is wrong");
+		assertEquals("21-09-2025", rec.candidateApplicationFromDatePickup("21-09-2025"), "To Date is wrong");
+	}
+
+	@Test(groups = { "regression" }, priority = 7)
+	void methodOfApplicationSelection() {
+		assertEquals("Manual", rec.methodOfApplicationSelection("Manual"), "Method of Application selected is wrong");
+	}
+
+	@Test(groups = { "sanity" }, priority = 8)
+	void searchButtonClick() {
+		assertEquals("search", rec.requirmentPageButtons("serach"), "Search Button is not clicked");
+
+	}
+	
+	@Test(groups = { "sanity" }, priority = 8)
+	void addButtonClick() {
+		assertEquals("add", rec.requirmentPageButtons("add"), "add Button is not clicked");
+
+	}
+	
+	@Test(groups = { "sanity" }, priority = 8)
+	void resetButtonClick() {
+		assertEquals("reset", rec.requirmentPageButtons("reset"), "reset Button is not clicked");
+
+	}
 }
