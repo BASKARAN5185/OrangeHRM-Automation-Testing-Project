89fd3180f93955809dc00d233ce6e142d32c65c3
BASKARAN5185 <baskarbala5185@gmail.com>
2025-11-16 12:49:30 +0530

    feat(addvacancy): some action methods are added

--- src/test/java/pageObjectClass/AddVacancyPage.java
+++ src/test/java/pageObjectClass/AddVacancyPage.java
@@ -1,14 +1,22 @@
 package pageObjectClass;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddVacancyPage {
   
     private RemoteWebDriver driver;
-    
+    private WebDriverWait wait;
     public AddVacancyPage(RemoteWebDriver driver) {
         this.driver = driver;
+        this.wait=new WebDriverWait(driver,Duration.ofSeconds(10));
     }
 
     // Add Vacancy Page locators
@@ -22,5 +30,29 @@ public class AddVacancyPage {
 	private By publishInRSSCheckbox = By
 			.xpath("//(span[contains(@class,'oxd-switch-input oxd-switch-input--active --label-right')[2]]");
     private By cancelButton = By.xpath("//button[contains(.,'Cancel')]");
-    private By saveButton = By.xpath("//button[text()=' Save ']");        
+    private By saveButton = By.xpath("//button[text()=' Save ']");  
+    
+    // Methods to interact with Add Vacancy Page elements
+    public void enterVacancyName(String name) {
+        WebElement vacancyNameInput = driver.findElement(vacancyName);
+        vacancyNameInput.clear();
+        vacancyNameInput.sendKeys(name);
+    }
+
+    public void clickSaveButton() {
+        WebElement saveBtn = wait.until(ExpectedConditions.elementToBeClickable(saveButton));
+        saveBtn.click();
+    }
+
+    public void selectVacancyTitle(){
+		WebElement statusDropdown=driver.findElement(addVacancyJobTitle);
+		statusDropdown.click();
+		Actions action=new Actions(driver);
+		action.sendKeys(Keys.PAGE_DOWN).perform();
+		action.sendKeys(Keys.PAGE_DOWN).perform();
+		action.sendKeys(Keys.ENTER).perform();
+	}
+
+
+    
 }
