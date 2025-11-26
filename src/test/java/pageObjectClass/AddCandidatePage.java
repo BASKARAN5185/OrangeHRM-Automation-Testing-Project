4f0b9404d49fe51f18c6f6984903cfa581b24b64
BASKARAN5185 <baskarbala5185@gmail.com>
2025-11-26 21:44:18 +0530

    feat: implement AddCandidatePage methods with explicit waits and add email and vacancystatus method

--- src/test/java/pageObjectClass/AddCandidatePage.java
+++ src/test/java/pageObjectClass/AddCandidatePage.java
@@ -1,15 +1,23 @@
 package pageObjectClass;
 
+import java.time.Duration;
+
+import org.jspecify.annotations.NonNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddCandidatePage {
 
     private RemoteWebDriver driver;
+    private WebDriverWait wait;
     public AddCandidatePage(RemoteWebDriver driver) {
         this.driver = driver;
+        this.wait=new WebDriverWait(driver,Duration.ofSeconds(10)); 
     }
 
     // Locators
@@ -30,26 +38,47 @@ public class AddCandidatePage {
     // Actions
     public void enterFirstName(String fname) {
         WebElement element = driver.findElement(firstName);
+        wait.until(ExpectedConditions.elementToBeClickable(element));      
         element.clear();
         element.sendKeys(fname);
     }
 
     public void enterMiddlename(String mname) {
         WebElement midName = driver.findElement(middleName);
+        wait.until(ExpectedConditions.elementToBeClickable(middleName));
         midName.clear();
         midName.sendKeys(mname);
     }
 
     public void enterLastName(String lName) {
         WebElement lastNameField = driver.findElement(lastName);
+        wait.until(ExpectedConditions.elementToBeClickable(lastNameField));
         lastNameField.clear();
         lastNameField.sendKeys(lName);
     }
 
     public String enterTitle(String title) {
         WebElement jobTitle = driver.findElement(fromTitle);
+        wait.until(ExpectedConditions.elementToBeClickable(jobTitle));
         jobTitle.sendKeys(Keys.chord(Keys.SHIFT, "a"));
         jobTitle.sendKeys(title);
         return jobTitle.getText();
+    }
+
+    public String selectVacancyType(){
+        WebElement vacancyname= driver.findElement(vacancy);
+        wait.until(ExpectedConditions.elementToBeClickable(vacancyname));
+        Actions dropdown =new Actions(driver); 
+        dropdown.sendKeys(Keys.PAGE_DOWN).build();
+        dropdown.sendKeys(Keys.PAGE_DOWN).build();
+        dropdown.sendKeys(Keys.ENTER);
+        return vacancyname.getText();
+    }
+
+    public String enterEmail(String emailadd){
+         WebElement enterMail= driver.findElement(email);
+         wait.until(ExpectedConditions.elementToBeClickable(enterMail));
+         enterMail.sendKeys(emailadd);
+        return enterMail.getText(); 
     }
 }
