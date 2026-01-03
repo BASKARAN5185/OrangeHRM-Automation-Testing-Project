63fd23fb5ff81713ed0ab924a8cb7c6adf84b703
BASKARAN5185 <baskarbala5185@gmail.com>
2026-01-03 18:42:18 +0530

    feat(admin): add Job title page title verification, add button click, record count extraction, and job title collection methods

--- src/test/java/pageObjectClass/admin/JobTitlesPage.java
+++ src/test/java/pageObjectClass/admin/JobTitlesPage.java
@@ -1,8 +1,14 @@
 package pageObjectClass.admin;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class JobTitlesPage {
@@ -20,4 +26,39 @@ public class JobTitlesPage {
     private final By jobTitleNames=By.xpath("//div[@role='row']//div[@class='data']");
     private final By deleteIcon=By.className("oxd-icon bi-trash");
     private final By editIcon=By.className("oxd-icon bi-pencil-fill");
+
+    public String verifyThePageTitle(){
+        String pagename = "";
+        try {
+           WebElement title=wait.until(ExpectedConditions.visibilityOfElementLocated(pageOfTitle));
+           pagename= title.getText();
+        } catch (Exception e) {
+          System.err.println("Filed to Retrive the page title"+e.getMessage());
+        }
+        return pagename;
+    }
+
+    public boolean jobTitlePageAddButtonClick(){
+      WebElement addButtoElement=wait.until(ExpectedConditions.elementToBeClickable(jobTitleAddButton));
+      addButtoElement.click();
+      return true;
+    }
+
+    public int getNumberOfRecordsFound(){
+        String recocrdString=driver.findElement(recordsFound).getText().trim().replaceAll("[^0-9]", "");
+        return Integer.parseInt(recocrdString);
+    }
+
+    // helper methods
+    private List<String> getJobTitleAll(){
+       List<String> allNames = new ArrayList<>();
+       List<WebElement> titleElements= driver.findElements(jobTitleNames);
+       int sizeOFTitle=titleElements.size();
+       for(int i=sizeOFTitle; i >= 0;--i){
+           String name=titleElements.get(i).getText().trim();
+           allNames.add(name);
+       }
+       return allNames;
+    }
+
 }
